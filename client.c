958dfaed2597ea521611dab2f36fb7fab3eb379f
Aaron Pryce <aapryce@a2-r7-p1.s19.be>
2024-02-07 16:09:03 +0100

    beginning

--- client.c
+++ client.c
@@ -1,47 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aapryce <aapryce@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/06 14:27:01 by aapryce           #+#    #+#             */
-/*   Updated: 2024/02/07 16:06:46 by aapryce          ###   ########.fr       */
+/*   Created: 2024/02/07 16:05:52 by aapryce           #+#    #+#             */
+/*   Updated: 2024/02/07 16:06:47 by aapryce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ascii_2_bin(const char *str)
+int        main(void)
 {
-	int	i;
-	int	j;
-	int	len;
-	int	res;
-	unsigned char	c;
 
-	len = ft_strlen(str);
-	i = 0;
-	while (i < len)
-	{
-		c = str[i];
-		j = 7;
-		while (j >= 0)
-		{
-			ft_printf("%d", (c >> j) & 1);
-			j--;
-		}
-		ft_printf(" ");
-		i++;
-	}
-	ft_printf("\n");
-}
-
-int	main(void)
-{
-	char	*str;
-
-	str = "Trance";
-	ascii_2_bin(str);
-	return(0);
 }
--- minitalk.h
+++ minitalk.h
@@ -3,17 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aapryce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aapryce <aapryce@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 13:35:20 by aapryce           #+#    #+#             */
-/*   Updated: 2024/02/06 14:46:59 by aapryce          ###   ########.fr       */
+/*   Updated: 2024/02/07 16:06:48 by aapryce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK.H
-# define MINITALK.H
+#ifndef MINITALK_H
+# define MINITALK_H
 
 # include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
 # include <signal.h>
+# include <unistd.h>
+
+void	ascii_2_bin(const char *str);
 
 #endif
